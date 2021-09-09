0116665ec9164a8f9446a996d20922ec376b1a29
11739266@qq.com <11739266@qq.com>
2021-09-09 21:37:41 +0800

    3

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/1',
+    name: '1',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "1" */'../views/1.vue')
   }
 ]
 
---
+++ src/views/1.vue
@@ -0,0 +1,12 @@
+
+<template>
+<div>
+
+good
+
+</div>
+</template>
+<script>
+</script>
+<style>
+</style>
