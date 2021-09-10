3e20ab098af26625c2c8b685462c9baf50a660f6
youjunchang <11739266@qq.com>
2021-09-10 17:41:32 +0800

    910

--- src/router/index.js
+++ src/router/index.js
@@ -30,6 +30,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import( /* webpackChunkName: "1" */'../views/2.vue')
+  },
+  {
+    path: '/3',
+    name: '3',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "1" */'../views/3.vue')
+  },
+  {
+    path: '/4',
+    name: '4',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "1" */'../views/4.vue')
   }
 ]
 
