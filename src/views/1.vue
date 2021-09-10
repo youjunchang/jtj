4266874fc134e00637f6c3b27cd55123f489c05b
youjunchang <11739266@qq.com>
2021-09-10 10:09:19 +0800

    good

--- src/views/1.vue
+++ src/views/1.vue
@@ -1,12 +1,33 @@
+<!--  -->
 <template>
- 
-<div>
+  <div>
 
-good
-
-</div>
+      非常好的东西
+  </div>
 </template>
-<script  >
+
+<script lang='ts'>
+import { reactive,toRefs,onBeforeMount,onMounted} from 'vue'
+export default {
+    name: '',
+      setup() {
+          console.log('1-开始创建组件-setup')
+          const data= reactive({
+
+          })
+          onBeforeMount(() => {
+              console.log('2.组件挂载页面之前执行----onBeforeMount')
+          })
+          onMounted(() => {
+              console.log('3.-组件挂载到页面之后执行-------onMounted')
+          })
+          const refData = toRefs(data);
+          return {
+              ...refData,
+          }
+
+      }
+  };
 </script>
-<style>
+<style lang='scss' scoped>
 </style>
