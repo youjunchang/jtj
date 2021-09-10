807adab01ff8b76775c506f2e39ea8fd245de6f2
youjunchang <11739266@qq.com>
2021-09-10 12:12:04 +0800

    910

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import( /* webpackChunkName: "1" */'../views/1.vue')
+  },
+  {
+    path: '/2',
+    name: '2',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "1" */'../views/2.vue')
   }
 ]
 
--- src/views/2.vue
+++ src/views/2.vue
@@ -1,21 +1,19 @@
 <!--  -->
 <template>
   <div>
-  {{msg}}
-      非常好的东西
-      <button @click="jump" >路由跳转</button>
+
+kaishi  ceshi 
+
   </div>
 </template>
 
 <script lang='ts'>
 import { reactive,toRefs,onBeforeMount,onMounted} from 'vue'
-import {useRouter } from "vue-router"
 export default {
     name: '',
       setup() {
           console.log('1-开始创建组件-setup')
           const data= reactive({
-              msg:'fuckyou 你老母'
 
           })
           onBeforeMount(() => {
@@ -24,13 +22,9 @@ export default {
           onMounted(() => {
               console.log('3.-组件挂载到页面之后执行-------onMounted')
           })
-          const router=useRouter();
-          let jump=()=>{
-           router.push({name:'2',params:{userid:'2'}})
-          }
           const refData = toRefs(data);
           return {
-              ...refData,jump
+              ...refData,
           }
 
       }
